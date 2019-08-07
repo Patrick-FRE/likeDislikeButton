5c2cadd6c3dd4983aa56f7d99c30c8428551049a
Yuhan Lin <yuhanlin@Yuhans-MacBook-Pro.local>
2019-08-07 16:32:23 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,83 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'likeDislikeButton';
+  counter: {
+    like: {
+      count: number;
+      selected: boolean;
+    };
+    dislike: {
+      count: number;
+      selected: boolean;
+    };
+  } = {
+    like: {
+      count: 99,
+      selected: false
+    },
+    dislike: {
+      count: 99,
+      selected: false
+    }
+  };
+
+  constructor() {}
+
+  clickHandler(likeOrDislike) {
+    switch (likeOrDislike) {
+      case 'like':
+        this.likeClicked(this.counter);
+        break;
+      case 'dislike':
+        this.dislikeClicked(this.counter);
+        break;
+      default:
+        console.log('error');
+    }
+  }
+
+  likeClicked(counter: {
+    like: {
+      count: number;
+      selected: boolean;
+    };
+    dislike: {
+      count: number;
+      selected: boolean;
+    };
+  }) {
+    if (counter.like.selected === true) {
+      counter.like.count--;
+      counter.like.selected = false;
+    } else {
+      counter.like.count++;
+      counter.like.selected = true;
+      if (counter.dislike.selected === true) {
+        counter.dislike.count--;
+        counter.dislike.selected = false;
+      }
+    }
+  }
+  dislikeClicked(counter: {
+    like: {
+      count: number;
+      selected: boolean;
+    };
+    dislike: {
+      count: number;
+      selected: boolean;
+    };
+  }) {
+    if (counter.dislike.selected === true) {
+      counter.dislike.count--;
+      counter.dislike.selected = false;
+    } else {
+      counter.dislike.count++;
+      counter.dislike.selected = true;
+      if (counter.like.selected === true) {
+        counter.like.count--;
+        counter.like.selected = false;
+      }
+    }
+  }
 }
